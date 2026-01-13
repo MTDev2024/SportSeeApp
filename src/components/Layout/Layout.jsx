01e556fc9cbd779d5b55f8114ed45f13d62dcb54
MTDev2024 <mt.dev.2023@gmail.com>
2026-01-13 03:34:10 +0100

    docs: ajout JSDoc et PropTypes sur Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,14 @@
+import PropTypes from "prop-types";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+
+/**
+ * Composant Layout - Structure globale de l'application
+ * Contient le Header, la Sidebar et la zone de contenu principale
+ *
+ * @param {Object} props
+ * @param {React.ReactNode} props.children - Contenu de la page à afficher
+ */
 
 function Layout({ children }) {
   return (
@@ -10,5 +19,10 @@ function Layout({ children }) {
     </div>
   );
 }
+
+// Validation des props
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Layout;
